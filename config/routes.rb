374778e5ca0598209cf61494798745831a43b77f
Ismail Eid <ismacileid122@gmail.com>
2021-02-28 14:38:45 +0300

    error solving1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#Home'             # Home page [server side]
-  
-  get '/' => 'static_pages#Home'
+
   get '/signup' => 'static_pages#Home' # Signup page [client side]
   get '/login' => 'static_pages#Home'  # Login page [client side]
   get '/about' => 'static_pages#Home'  # About page [client side]
