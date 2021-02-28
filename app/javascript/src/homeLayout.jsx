3393fb160d81b4a9d19728d506ce84a9331e12f9
Ismail Eid <ismacileid122@gmail.com>
2021-02-28 14:55:41 +0300

    solving a new problem

--- app/javascript/src/homeLayout.jsx
+++ app/javascript/src/homeLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import './homeLayout.scss'
+
 const Layout = (props) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Dugsi Cloud</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <a className="navbar-brand" href="#">Dugsi Cloud</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
         <div className="collapse navbar-collapse justify-content-end">
           <ul className="navbar-nav">
