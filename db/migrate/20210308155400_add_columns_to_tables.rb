8c70081c3bc8d1397a596273cacc4f2456f27ee6
Ismail Eid <ismacileid122@gmail.com>
2021-03-20 18:13:27 +0300

    error solving

--- db/migrate/20210308155400_add_columns_to_tables.rb
+++ db/migrate/20210308155400_add_columns_to_tables.rb
@@ -1,6 +1,7 @@
 class AddColumnsToTables < ActiveRecord::Migration[6.0]
   def change
-    add_column :students, :student_id, :string 
+    add_column :students, :student_id, :string
+    add_belongs_to :parent, index: true, foreign_key: true
     
   end
 end
