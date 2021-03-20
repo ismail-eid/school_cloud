0357b08b62f6bbf07b28b6ce579a0ab3e5ac1fc5
Ismail Eid <ismacileid122@gmail.com>
2021-03-20 20:43:45 +0300

    hello

--- db/migrate/20210307134531_create_parentusers.rb
+++ db/migrate/20210307134531_create_parentusers.rb
@@ -1,12 +1,4 @@
 class CreateParentusers < ActiveRecord::Migration[6.0]
   def change
-    create_table :parentusers do |t|
-      t.string :username
-      t.string :password
-
-      t.belongs_to :parent, index: true, foreign_key: true
-  
-      t.timestamps
-    end
   end
 end
