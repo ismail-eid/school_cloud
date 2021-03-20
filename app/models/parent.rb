49144b9ff93f8620e423d81275d105b2ec0fd556
Ismail Eid <ismacileid122@gmail.com>
2021-03-20 17:54:29 +0300

    error solving

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,7 @@
 class Parent < ApplicationRecord
   belongs_to :school
   has_many :students
+  has_many :parentusers
 
   validates :full_name, presence: true, length: { minimum: 8, maximum: 64 }
   validates :phone, presence: true, length: {minimum: 10, maximum: 20}
